b480af49cd601d71595a3726c391375d5532aac6
Artem Mezin <iketari@gmail.com>
2016-04-01 21:12:14 +0300

    game field

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,24 +1,31 @@
 module.exports = function(grunt) {
 
-  grunt.initConfig({
-    jshint: {
-      files: ['Gruntfile.js', 'assets/**/*.js'],
-      options: {
-        globals: {
-          jQuery: true
+    grunt.initConfig({
+        jshint: {
+        files: ['Gruntfile.js', 'assets/**/*.js'],
+        options: {
+            globals: {
+                jQuery: true
+            }
+            }
+        },
+        watch: {
+            files: ['<%= jshint.files %>'],
+            tasks: ['jshint']
+        },
+        stylus: {
+            compile: {
+                files: {
+                    'assets/style/main.css': 'assets/style/main.styl'
+                }    
+            }
         }
-      }
-    },
-    watch: {
-      files: ['<%= jshint.files %>'],
-      tasks: ['jshint']
-    }
-  });
+    });
 
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-stylus');
-  grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-stylus');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
-  grunt.registerTask('default', ['jshint']);
+    grunt.registerTask('default', ['jshint']);
 
 };
